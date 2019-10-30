9411cdec7ad2c86db15f4ddbd69649db36682dfe
Simon Ammer <ammersimon@gmail.com>
2019-10-30 15:47:29 +0100

    🏗 Add projects to solution

---
+++ Hurace/Hurace.Domain/DataObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hurace.Domain
+{
+    public abstract class DataObject
+    {
+        public int Id
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
