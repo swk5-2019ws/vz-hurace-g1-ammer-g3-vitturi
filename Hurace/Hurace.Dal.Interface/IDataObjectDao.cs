7b1c76a365bac29ee58e07ae731fa6e304705531
Jan Vitturi <vitturi.jan@gmail.com>
2019-11-03 22:17:39 +0100

    Add DAO interfaces

---
+++ Hurace/Hurace.Dal.Interface/IDataObjectDao.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hurace.Domain;
+
+namespace Hurace.Dal.Interface
+{
+    public interface IDataObjectDao<T> where T : DataObject
+    {
+        Task<IEnumerable<T>> FindAll();
+        Task<T> FindById(int id);
+        Task<bool> Update(T dataObject);
+    }
+}
---
+++ Hurace/Hurace.Dal.Interface/IRaceDataDao.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hurace.Domain;
+
+namespace Hurace.Dal.Interface
+{
+    public interface IRaceDataDao : IDataObjectDao<RaceData>
+    {
+    }
+}
