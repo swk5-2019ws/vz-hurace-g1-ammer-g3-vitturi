08f347143460b2af482d301e7ff8a905c04a14a8
Simon Ammer <ammersimon@gmail.com>
2019-11-06 18:43:28 +0100

    🏗 Add core mapper project

---
+++ Hurace/Hurace.Mapper/Attributes/Table.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hurace.Core.Mapper.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class Table : Attribute
+    {
+        public Table(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
