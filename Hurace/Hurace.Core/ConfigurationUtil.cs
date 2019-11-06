49cf12497c824a218f687e1548839f9fbae11e17
Jan Vitturi <vitturi.jan@gmail.com>
2019-11-06 21:46:43 +0100

    Add database connection

---
+++ Hurace/Hurace.Core/ConfigurationUtil.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Hurace.Core
+{
+    public static class ConfigurationUtil
+    {
+        private static IConfiguration configuration = null;
+
+        public static IConfiguration GetConfiguration() =>
+          configuration = configuration ?? new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+        public static string GetConnectionString(string configName) =>
+            GetConfiguration()
+            .GetSection("ConnectionStrings")
+            .GetSection(configName).Value;
+    }
+}
---
+++ Hurace/Hurace.Core/ConnectionFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Hurace.Core
+{
+    public class ConnectionFactory
+    {
+        public string ConnectionString { get; }
+
+        public ConnectionFactory(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            IDbConnection connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            InitializeDatabase(connection);
+            return connection;
+        }
+
+        private void InitializeDatabase(IDbConnection connection)
+        {
+            using (IDbCommand command = connection.CreateCommand())
+            {
+                string script = File.ReadAllText(".\\Scripts\\create_tables.sql");
+                command.CommandText = script;
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
