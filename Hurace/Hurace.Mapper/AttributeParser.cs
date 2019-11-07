d029302375cd35276affeb587604e6f885bfea78
Simon Ammer <ammersimon@gmail.com>
2019-11-07 20:08:39 +0100

    🗃 Add GetAll and Get extension methods

---
+++ Hurace/Hurace.Mapper/AttributeParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Hurace.Core.Mapper.Attributes;
+
+namespace Hurace.Core.Mapper
+{
+    internal class AttributeParser
+    {
+        /// <summary>
+        ///     Returns the table name of a type. If the Table is not found the type name will be used.
+        /// </summary>
+        /// <param name="type">The type</param>
+        /// <returns>The table name of the type.</returns>
+        internal static string GetTableName(Type type)
+        {
+            var tableName = nameof(type);
+            var tableAttributes = (Table[]) type.GetCustomAttributes(typeof(Table), false);
+
+            if (tableAttributes.Length > 0) tableName = tableAttributes[0].Name;
+
+            return tableName;
+        }
+
+        /// <summary>
+        ///     Returns the name of the property with a key attribute. If no or multiple keys are defined an exception is thrown.
+        /// </summary>
+        /// <param name="type">The type</param>
+        /// <returns>The property name with the key attribute</returns>
+        public static string GetKey(Type type)
+        {
+            var keyProperties = type.GetProperties().Where(property => Attribute.IsDefined(property, typeof(Key)))
+                .ToArray();
+
+            if (keyProperties.Length > 1)
+                throw new DataException("The mapper only supports an entity with a single key!");
+            if (keyProperties.Length == 0)
+                throw new DataException("The mapper only supports an entity with a key!");
+
+            return keyProperties[0].Name;
+        }
+    }
+}
--- Hurace/Hurace.Mapper/MapperExtensions.cs
+++ Hurace/Hurace.Mapper/MapperExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Hurace.Core.Mapper
@@ -8,34 +9,46 @@ namespace Hurace.Core.Mapper
     public static class MapperExtensions
     {
         /// <summary>
-        /// Returns a list of entities.  
+        ///     Returns a list of entities.
         /// </summary>
         /// <typeparam name="TEntity">Entity type</typeparam>
-        /// <param name="connection">Open SqlConnection</param>
+        /// <param name="connection">Open DbConnection</param>
         /// <returns>Entities of Type TEntity</returns>
         public static Task<IEnumerable<TEntity>> GetAll<TEntity>(this DbConnection connection)
         {
-            return null;
+            var type = typeof(TEntity);
+            var tableName = AttributeParser.GetTableName(type);
+            var sql = $"SELECT * FROM {tableName}";
+
+            return connection.Query<TEntity>(sql);
         }
 
         /// <summary>
-        /// Returns a single entity by a single id.  
-        /// Id must be marked with [Key] attribute.
+        ///     Returns a single entity by a single id.
+        ///     Id must be marked with [Key] attribute.
         /// </summary>
         /// <typeparam name="TEntity">Entity type</typeparam>
-        /// <param name="connection">Open SqlConnection</param>
+        /// <param name="connection">Open DbConnection</param>
         /// <param name="id">Id of the entity to get, must be marked with [Key] attribute</param>
         /// <returns>Entity of Type TEntity</returns>
-        public static Task<TEntity> Get<TEntity>(this DbConnection connection, dynamic id)
+        public static async Task<TEntity> Get<TEntity>(this DbConnection connection, dynamic id)
         {
-            return null;
+            var type = typeof(TEntity);
+            var tableName = AttributeParser.GetTableName(type);
+            var key = AttributeParser.GetKey(type);
+            var sql = $"SELECT * FROM {tableName} WHERE {key} = @id";
+
+            IDictionary<string, object> param = new Dictionary<string, object>();
+            param["@id"] = id;
+
+            return (await connection.Query<TEntity>(sql, param)).FirstOrDefault();
         }
 
         /// <summary>
-        /// Inserts an entity and returns an id.
+        ///     Inserts an entity and returns an id.
         /// </summary>
         /// <typeparam name="TEntity">Entity type</typeparam>
-        /// <param name="connection">Open SqlConnection</param>
+        /// <param name="connection">Open DbConnection</param>
         /// <param name="entity">The new entity.</param>
         /// <returns>Id of inserted entity</returns>
         public static Task<TEntity> Insert<TEntity>(this DbConnection connection, TEntity entity)
@@ -44,10 +57,10 @@ namespace Hurace.Core.Mapper
         }
 
         /// <summary>
-        /// Updates an entity.
+        ///     Updates an entity.
         /// </summary>
         /// <typeparam name="TEntity">Entity type</typeparam>
-        /// <param name="connection">Open SqlConnection</param>
+        /// <param name="connection">Open DbConnection</param>
         /// <param name="entity">The updated entity.</param>
         /// <returns>True if updated. Otherwise false.</returns>
         public static Task<bool> Update<TEntity>(this DbConnection connection, TEntity entity)
@@ -56,15 +69,42 @@ namespace Hurace.Core.Mapper
         }
 
         /// <summary>
-        /// Deletes an entity.
+        ///     Deletes an entity.
         /// </summary>
         /// <typeparam name="TEntity">Entity type</typeparam>
-        /// <param name="connection">Open SqlConnection</param>
+        /// <param name="connection">Open DbConnection</param>
         /// <param name="id">Id of the entity to delete, must be marked with [Key] attribute</param>
         /// <returns>True if deleted. Otherwise false if not found.</returns>
         public static Task<bool> Delete<TEntity>(this DbConnection connection, dynamic id)
         {
             return null;
+        }
+
+        /// <summary>
+        ///     Execute a query.
+        /// </summary>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <param name="connection">Open DbConnection</param>
+        /// <param name="sql">The SQL to execute</param>
+        /// <param name="param">The parameters for the query</param>
+        /// <returns>The entities.</returns>
+        public static async Task<IEnumerable<TEntity>> Query<TEntity>(this IDbConnection connection, string sql,
+            IDictionary<string, object> param = null)
+        {
+            return null;
+        }
+
+        /// <summary>
+        ///     Execute a command.
+        /// </summary>
+        /// <param name="connection">Open DbConnection</param>
+        /// <param name="sql">The SQL to execute</param>
+        /// <param name="param">The parameters for the query</param>
+        /// <returns>The number of rows affected.</returns>
+        public static async Task<int> ExecuteAsync(this IDbConnection connection, string sql,
+            IDictionary<string, object> param = null)
+        {
+            return 0;
         }
     }
 }
