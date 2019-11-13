47b152afdc8070e9aecfb10897c1a1d73d67e717
Jan Vitturi <vitturi.jan@gmail.com>
2019-11-13 21:02:44 +0100

    Change data model

--- Hurace/Hurace.Core/Scripts/create_tables.sql
+++ Hurace/Hurace.Core/Scripts/create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS races
+CREATE TABLE IF NOT EXISTS race
 (
     id                INTEGER PRIMARY KEY,
     date              TEXT    NOT NULL,
@@ -6,101 +6,103 @@ CREATE TABLE IF NOT EXISTS races
     name              TEXT    NOT NULL,
     number_of_sensors INTEGER NOT NULL,
     website           TEXT,
-    gender_id         TEXT,
-    competition_id    INTEGER,
-    FOREIGN KEY (gender_id) REFERENCES genders (id),
-    FOREIGN KEY (competition_id) REFERENCES competitions (id),
+    gender_name       TEXT,
+    location_id    INTEGER,
+    race_type_name TEXT,
+    FOREIGN KEY (gender_name) REFERENCES gender (name),
+    FOREIGN KEY (race_type_name) REFERENCES race_type (name)
+    FOREIGN KEY (location_id) REFERENCES location (id),
     CHECK (number_of_sensors > 0)
 );
 
-CREATE TABLE IF NOT EXISTS skiers
+CREATE TABLE IF NOT EXISTS skier
 (
     id            INTEGER PRIMARY KEY,
     archived      INTEGER DEFAULT 0,
     birthdate     TEXT NOT NULL,
     first_name    TEXT NOT NULL,
     last_name     TEXT NOT NULL,
-    profile_image BLOB,
+    picture_url   TEXT,
     national_code TEXT,
-    gender_id     TEXT,
-    FOREIGN KEY (national_code) REFERENCES countries (national_code),
-    FOREIGN KEY (gender_id) REFERENCES genders (id),
+    gender_name     TEXT,
+    FOREIGN KEY (national_code) REFERENCES country(national_code),
+    FOREIGN KEY (gender_name) REFERENCES gender (name),
     CHECK ( archived = 0 OR archived = 1 )
 );
 
-CREATE TABLE IF NOT EXISTS start_lists
+CREATE TABLE IF NOT EXISTS skier_run
 (
-    id       INTEGER PRIMARY KEY,
-    number   INTEGER,
-    skier_id INTEGER,
-    race_id  INTEGER,
-    run_id   INTEGER,
-    FOREIGN KEY (skier_id) REFERENCES skiers (id),
-    FOREIGN KEY (race_id) REFERENCES races (id),
-    FOREIGN KEY (run_id) REFERENCES runs (id),
+    skier_id   INTEGER,
+    race_id    INTEGER,
+    run_number INTEGER,
+    PRIMARY KEY (skier_id, race_id, run_number),
+    FOREIGN KEY (skier_id) REFERENCES skier (id),
+    FOREIGN KEY (race_id) REFERENCES race (id),
+    CHECK ( run_number = 1 OR run_number = 2 )
+);
+
+CREATE TABLE IF NOT EXISTS start_list
+(
+    id         INTEGER PRIMARY KEY,
+    number     INTEGER,
+    skier_id   INTEGER,
+    race_id    INTEGER,
+    run_number INTEGER,
+    FOREIGN KEY (skier_id) REFERENCES skier (id),
+    FOREIGN KEY (race_id) REFERENCES race (id),
+    FOREIGN KEY (run_number) REFERENCES skier_run (run_number),
     CHECK (number > 0)
 );
 
 CREATE TABLE IF NOT EXISTS race_data
 (
-    id             INTEGER PRIMARY KEY,
-    position       INTEGER,
-    run_id         INTEGER,
-    race_status_id INTEGER,
-    FOREIGN KEY (run_id) REFERENCES runs (id),
-    FOREIGN KEY (race_status_id) REFERENCES race_status (id),
-    CHECK (position > 0)
+    id          INTEGER PRIMARY KEY,
+    time        REAL,
+    race_status TEXT,
+    skier_id    INTEGER,
+    race_id     INTEGER,
+    run_number  INTEGER,
+    FOREIGN KEY (skier_id) REFERENCES skier (id),
+    FOREIGN KEY (race_id) REFERENCES race (id),
+    FOREIGN KEY (run_number) REFERENCES skier_run (run_number),
+    CHECK (
+        race_status IS NULL OR
+        race_status = 'DNF' OR
+        race_status = 'DNQ'
+    )
 );
 
-CREATE TABLE IF NOT EXISTS sensor_data
+CREATE TABLE IF NOT EXISTS sensor_measurement
 (
     id           INTEGER PRIMARY KEY,
     sensor_id    INTEGER NOT NULL,
-    time         TEXT    NOT NULL,
+    interim_time TEXT    NOT NULL,
     race_data_id INTEGER NOT NULL,
     FOREIGN KEY (race_data_id) REFERENCES race_data (id),
     CHECK (sensor_id >= 0)
 );
 
-CREATE TABLE IF NOT EXISTS locations
+CREATE TABLE IF NOT EXISTS location
 (
     id            INTEGER PRIMARY KEY,
     name          TEXT UNIQUE NOT NULL,
-    national_code INTEGER,
-    FOREIGN KEY (national_code) REFERENCES countries (national_code)
+    code INTEGER,
+    FOREIGN KEY (code) REFERENCES country (code)
 );
 
-CREATE TABLE IF NOT EXISTS competitions
+CREATE TABLE IF NOT EXISTS country
 (
-    id           INTEGER PRIMARY KEY,
-    location_id  INTEGER,
-    race_type_id INTEGER,
-    FOREIGN KEY (location_id) REFERENCES locations (id),
-    FOREIGN KEY (race_type_id) REFERENCES race_types (id)
+    code TEXT PRIMARY KEY NOT NULL,
+    CHECK (LENGTH(code) = 3)
 );
 
-CREATE TABLE IF NOT EXISTS countries
+CREATE TABLE IF NOT EXISTS race_type
 (
-    national_code TEXT PRIMARY KEY NOT NULL,
-    CHECK (LENGTH(national_code) = 3)
+    name TEXT PRIMARY KEY NOT NULL,
+    run_count INTEGER NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS race_types
+CREATE TABLE IF NOT EXISTS gender
 (
-    id TEXT PRIMARY KEY NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS genders
-(
-    id TEXT PRIMARY KEY NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS runs
-(
-    id INTEGER PRIMARY KEY NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS race_status
-(
-    id TEXT PRIMARY KEY NOT NULL
+    name TEXT PRIMARY KEY NOT NULL
 );
