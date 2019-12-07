af1043badc1e19ecabdf67acc1610e8e12e64a56
Jan Vitturi <vitturi.jan@gmail.com>
2019-12-07 12:50:45 +0100

    Add SkierDao search method

--- Hurace/Hurace.Core.Interface/ISkierDao.cs
+++ Hurace/Hurace.Core.Interface/ISkierDao.cs
@@ -1,8 +1,14 @@
-﻿using Hurace.Domain;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hurace.Domain;
 
+#nullable enable
 namespace Hurace.Core.Interface
 {
     public interface ISkierDao : IDataObjectDao<Skier>
     {
+        Task<IEnumerable<Skier>> SearchSkiers(string? nameSubstring = null, Country? country = null,
+            Gender? gender = null);
     }
 }
--- Hurace/Hurace.Core/Daos/SkierDao.cs
+++ Hurace/Hurace.Core/Daos/SkierDao.cs
@@ -1,12 +1,58 @@
-﻿using Hurace.Core.Interface;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Hurace.Core.Interface;
+using Hurace.Core.Mapper;
 using Hurace.Domain;
 
+#nullable enable
 namespace Hurace.Core.Daos
 {
     public class SkierDao : DataObjectDao<Skier>, ISkierDao
     {
         public SkierDao(ConnectionFactory connectionFactory) : base(connectionFactory)
         {
+        }
+
+        public async Task<IEnumerable<Skier>> SearchSkiers(string? nameSubstring = null, Country? country = null,
+            Gender? gender = null)
+        {
+            using var connection = ConnectionFactory.CreateConnection();
+            var query = new StringBuilder("SELECT * FROM skier ");
+
+            if (nameSubstring != null || country != null || gender != null)
+            {
+                query.Append("WHERE ");
+                bool multipleFilters = false;
+
+                if (nameSubstring != null)
+                {
+                    query.Append("(first_name || ' ' || last_name) LIKE ('%' || @NameSubstring || '%') ");
+                    multipleFilters = true;
+                }
+
+                if (country != null)
+                {
+                    if (multipleFilters) query.Append("AND ");
+                    query.Append("country_id = @CountryId ");
+                    multipleFilters = true;
+                }
+
+                if (gender != null)
+                {
+                    if (multipleFilters) query.Append("AND ");
+                    query.Append("gender = @Gender ");
+                }
+            }
+
+            return await connection.Query<Skier>(query.ToString(), new
+                {
+                    NameSubstring = nameSubstring,
+                    CountryId = country?.Id,
+                    Gender = gender,
+                }
+            ).ConfigureAwait(false);
         }
     }
 }
