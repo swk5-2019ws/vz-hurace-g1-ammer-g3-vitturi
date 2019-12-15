ebea2b2040d115181afa10c1067906c9a3645858
Jan Vitturi <vitturi.jan@gmail.com>
2019-12-15 13:13:01 +0100

    Add run service

---
+++ Hurace/Hurace.Core/Services/RunService.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Hurace.Domain;
+
+namespace Hurace.Core.Services
+{
+    public class RunService : Service
+    {
+        public RunService(DaoProvider daoProvider) : base(daoProvider)
+        {
+        }
+
+        public async Task SetRunStatus(Race race, int runNumber, Skier skier, RunStatus status)
+        {
+            Run run = await DaoProvider.RunDao.GetBySkierAndRace(race, runNumber, skier).ConfigureAwait(false);
+            run.Status = status;
+            await DaoProvider.RunDao.Update(run).ConfigureAwait(false);
+        }
+    }
+}
