c5bbd48be0fcc3061e89fa97d09cbef25831fbf9
Jan Vitturi <vitturi.jan@gmail.com>
2019-12-16 17:57:13 +0100

    Add simulator project

---
+++ Hurace/Hurace.Simulator/SimulatorRaceClock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Hurace.Timer;
+
+namespace Hurace.Simulator
+{
+    public class SimulatorRaceClock : IRaceClock
+    {
+        public event TimingTriggeredHandler TimingTriggered;
+    }
+}
