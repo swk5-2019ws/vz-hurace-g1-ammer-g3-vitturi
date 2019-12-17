97e39896b18bd81515c9b1bcc4e8050c5d9cbbf2
Simon Ammer <ammersimon@gmail.com>
2019-12-17 14:57:59 +0100

    Add simulator page to app

--- Hurace/Hurace.RaceControl/ViewModels/SettingsViewModel.cs
+++ Hurace/Hurace.RaceControl/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using Windows.ApplicationModel;
+using Hurace.RaceControl.Helpers;
+using MvvmCross.Commands;
+using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
 
 namespace Hurace.RaceControl.ViewModels
@@ -6,11 +9,20 @@ namespace Hurace.RaceControl.ViewModels
     public class SettingsViewModel : MvxViewModel
     {
         private string _versionDescription;
+        private IMvxNavigationService _navigationService;
+
+        public MvxCommand OpenSimulatorCommand { get; set; }
 
         public string VersionDescription
         {
             get => _versionDescription;
             set => SetProperty(ref _versionDescription, value);
+        }
+
+        public SettingsViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            OpenSimulatorCommand = new MvxCommand(OpenSimulator);
         }
 
         public override void Prepare()
@@ -26,6 +38,11 @@ namespace Hurace.RaceControl.ViewModels
             var version = packageId.Version;
 
             return $"{package.DisplayName} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+
+        public void OpenSimulator()
+        {
+            _navigationService.Navigate<SimulatorViewModel>();
         }
     }
 }
---
+++ Hurace/Hurace.RaceControl/Views/Simulator.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Hurace.RaceControl.Helpers;
+using Hurace.RaceControl.ViewModels;
+using MvvmCross.Platforms.Uap.Views;
+using MvvmCross.ViewModels;
+
+
+namespace Hurace.RaceControl.Views
+{
+    [MvxViewFor(typeof(SimulatorViewModel))]
+    [MvxWindowPresentation]
+    public sealed partial class Simulator : MvxWindowsPage
+    {
+        public Simulator()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
