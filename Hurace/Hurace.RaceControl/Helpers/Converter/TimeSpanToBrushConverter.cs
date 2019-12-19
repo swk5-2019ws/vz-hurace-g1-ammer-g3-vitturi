34ee20beb107b48a99b0aea006a87ce23eedcf50
Simon Ammer <ammersimon@gmail.com>
2019-12-19 20:54:02 +0100

    Add controls to current skier screen

---
+++ Hurace/Hurace.RaceControl/Helpers/Converter/TimeSpanToBrushConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Hurace.RaceControl.Helpers.Converter
+{
+    class TimeSpanToBrushConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            double time = (double) value;
+            var brush = new AcrylicBrush();
+            brush.BackgroundSource = AcrylicBackgroundSource.Backdrop;
+            brush.TintOpacity = 0.6;
+
+            if (time > 0.0)
+            {
+                brush.TintColor = Color.FromArgb(255, 255, 40, 37);
+                brush.FallbackColor = Color.FromArgb(255, 255, 40, 37);
+            }
+            else
+            {
+                brush.TintColor = Color.FromArgb(255, 0, 179, 65);
+                brush.FallbackColor = Color.FromArgb(255, 0, 179, 65);
+            }
+
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
