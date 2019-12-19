92119450aa21af74b27ea9f62b3d111cea56ad8e
Jan Vitturi <vitturi.jan@gmail.com>
2019-12-20 00:14:59 +0100

    Add events to RunService

--- Hurace/Hurace.Core/Services/RunService.cs
+++ Hurace/Hurace.Core/Services/RunService.cs
@@ -1,11 +1,22 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Hurace.Domain;
+using Hurace.Timer;
 
 namespace Hurace.Core.Services
 {
     public class RunService : Service
     {
-        public RunService(DaoProvider daoProvider) : base(daoProvider)
+        public delegate void SensorMeasurementAddedHandler(Race race, int runNumber, Skier skier, TimeSpan timeSpan);
+
+        public delegate void RunStatusChangedHandler(Race race, int runNumber, Skier skier, RunStatus runStatus);
+
+        public event SensorMeasurementAddedHandler SensorMeasurementAdded;
+
+        public event RunStatusChangedHandler RunStatusChanged;
+        
+        public RunService(DaoProvider daoProvider, IRaceClock raceClock) : base(daoProvider)
         {
         }
 
@@ -14,6 +25,8 @@ namespace Hurace.Core.Services
             Run run = await DaoProvider.RunDao.GetBySkierAndRace(race, runNumber, skier).ConfigureAwait(false);
             run.Status = status;
             await DaoProvider.RunDao.Update(run).ConfigureAwait(false);
+            RunStatusChanged?.Invoke(race, runNumber, skier, status);
+        }
 
         public async Task<IList<TimeSpan>> GetInterimTimes(Race race, int runNumber, Skier skier)
         {
