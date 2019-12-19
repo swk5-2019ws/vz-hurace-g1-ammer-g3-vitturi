4ba4d46b8ec85536518be709b6cf1d020ec8b714
Jan Vitturi <vitturi.jan@gmail.com>
2019-12-20 00:13:28 +0100

    Add GetInterimTimes to RunService

--- Hurace/Hurace.Core/Services/RunService.cs
+++ Hurace/Hurace.Core/Services/RunService.cs
@@ -14,6 +14,26 @@ namespace Hurace.Core.Services
             Run run = await DaoProvider.RunDao.GetBySkierAndRace(race, runNumber, skier).ConfigureAwait(false);
             run.Status = status;
             await DaoProvider.RunDao.Update(run).ConfigureAwait(false);
+
+        public async Task<IList<TimeSpan>> GetInterimTimes(Race race, int runNumber, Skier skier)
+        {
+            Run run = await DaoProvider.RunDao.GetBySkierAndRace(race, runNumber, skier).ConfigureAwait(false);
+            var sensorMeasurements =
+                await DaoProvider.SensorMeasurementDao.GetMeasurementForRun(run).ConfigureAwait(false);
+
+            IList<TimeSpan> interimTimes = new List<TimeSpan>();
+            foreach (var sensorMeasurement in sensorMeasurements)
+            {
+                int seconds = (int) sensorMeasurement.InterimTime;
+                int milliseconds = (int) ((sensorMeasurement.InterimTime - seconds) * 1000);
+
+                interimTimes.Add(new TimeSpan(
+                    days: 0, hours: 0, minutes: 0,
+                    seconds, milliseconds)
+                );
+            }
+
+            return interimTimes;
         }
     }
 }
