3b5281944a1e86fa7cd7f2e163e70b1df0d57d15
Simon Ammer <ammersimon@gmail.com>
2020-01-03 00:34:14 +0100

    Add Open API support

--- Hurace/Hurace.Api/Startup.cs
+++ Hurace/Hurace.Api/Startup.cs
@@ -1,8 +1,16 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Hurace.Core;
+using Hurace.Core.Daos;
+using Hurace.Core.Interface.Services;
+using Hurace.Core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Hurace.Api
 {
@@ -15,13 +23,30 @@ namespace Hurace.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hurace API", Version = "v1" }));
+            
+            AddHuraceServices(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        private void AddHuraceServices(IServiceCollection services)
+        {
+            var connectionString = ConfigurationReader.GetConnectionString(Environment.Production);
+            var connectionFactory = new ConnectionFactory(Environment.Production, connectionString);
+            var locationDao = new LocationDao(connectionFactory);
+            var skierDao = new SkierDao(connectionFactory);
+            var countryDao = new CountryDao(connectionFactory);
+            var raceDao = new RaceDao(connectionFactory);
+            var runDao = new RunDao(connectionFactory);
+            var sensorMeasurementDao = new SensorMeasurementDao(connectionFactory);
+            var daoProvider = new DaoProvider(countryDao, locationDao, raceDao, runDao, sensorMeasurementDao, skierDao);
+
+            services.AddSingleton<IRaceService>(new RaceService(daoProvider));
+            services.AddSingleton<ISkierService>(new SkierService(daoProvider));
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -31,6 +56,14 @@ namespace Hurace.Api
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hurace v1");
+                c.RoutePrefix = string.Empty;
+            });
+            
             app.UseRouting();
 
             app.UseAuthorization();
